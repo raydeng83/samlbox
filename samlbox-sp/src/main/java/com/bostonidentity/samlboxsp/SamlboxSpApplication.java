40a2dafe54a10e98033cd3b198708eec41909d35
raydeng83 <ray.deng83@gmail.com>
2025-02-11 16:08:20 -0500

    added SP

---
+++ samlbox-sp/src/main/java/com/bostonidentity/samlboxsp/SamlboxSpApplication.java
@@ -0,0 +1,13 @@
+package com.bostonidentity.samlboxsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamlboxSpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamlboxSpApplication.class, args);
+    }
+
+}
---
+++ samlbox-sp/src/test/java/com/bostonidentity/samlboxsp/SamlboxSpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bostonidentity.samlboxsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamlboxSpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
